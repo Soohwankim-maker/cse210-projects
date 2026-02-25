417e288b47c4b7017b8bd3860943fd24d98d8341
Soohwankim-maker <soohwan@byui.edu>
2026-02-25 10:16:36 -0700

    Changes to prep2 and sandbox

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,54 @@
 using System;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+
+        int grade;
+
+        Console.Write("Please enter your grade: ");
+        string responese = Console.ReadLine();
+        grade = int.Parse(responese);
+
+         string letter = "";
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+         Console.WriteLine($"Your grade is: {letter}");
+        
+        if (grade >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time!");
+        }
     }
+    
+    
+    
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,29 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Sandbox World!");
+        
+        int age;
+
+        Console.Write("Please enter your age: ");
+        string responese = Console.ReadLine();
+        age = int.Parse(responese);
+
+        Console.WriteLine($"Your age is: {age}");
+        int x = 20;
+        double z =12.23;
+    
+        if (age <= 100 && age >= 0 &&x <= 30 || z < 6)
+        {
+            Console.WriteLine("You are cool");
+        }
+        else if (age <= 100)
+        {
+            Console.WriteLine("You are sort of cool");
+        }
+
+        else 
+        {
+            Console.WriteLine("Wow you have had a lot of brithdays");
+        }
     }
 }
