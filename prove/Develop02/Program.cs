b21f0846685281616c5bfad5ea802631839c3c5b
Soohwankim-maker <soohwan@byui.edu>
2026-03-05 09:43:02 -0700

    working system

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -122,15 +122,15 @@ class Program
         Journal journal = new Journal();
         bool running = true;
 
-        Console.WriteLine("=== Welcome to the Journal Program ===");
+        Console.WriteLine("Welcome to the Journal Program");
 
         while (running)
         {
-            Console.WriteLine("\nMenu Options:");
-            Console.WriteLine("1. Write a new entry");
-            Console.WriteLine("2. Display the journal");
-            Console.WriteLine("3. Save the journal to a file");
-            Console.WriteLine("4. Load the journal from a file");
+            Console.WriteLine("Create, Display, Save, And Load Journal Entries");
+            Console.WriteLine("1. Create Journal Entry");
+            Console.WriteLine("2. Display Journal");
+            Console.WriteLine("3. Save Journal to file");
+            Console.WriteLine("4. Read Journal from file");
             Console.WriteLine("5. Quit");
             Console.Write("Select a choice from the menu: ");
 
