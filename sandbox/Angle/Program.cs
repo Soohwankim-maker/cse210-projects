26d16e7e1ba23a72a8b0d43014ac684c228937aa
Soohwankim-maker <soohwan@byui.edu>
2026-03-10 09:58:01 -0600

    new project

--- sandbox/Angle/Program.cs
+++ sandbox/Angle/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Angle myAngle = new Angle(180);
+        Console.Clear();
+
+        Console.WriteLine(myAngle.GetRadians());
+
+        Console.WriteLine(myAngle.GetDegrees());
+
+        myAngle.SetDegrees(270);
+
+        Console.WriteLine(myAngle.GetRadians());
     }
 }
